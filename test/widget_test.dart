44e80661f8cd0700905b2d056c2837c58602e710
KasenS1230 <kasen.sinclair@cox.net>
2026-09-15 16:00:23 -0500

    added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,4 +27,29 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+  testWidgets('Counter increment', (WidgetTester tester) async {
+  // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    final counter = find.byKey(const Key('counter-value'));
+  expect(tester.widget<Text>(counter).data, '0');
+
+  // // Verify that our counter starts at 0.
+  // expect(find.text('0'), findsOneWidget);
+  // expect(find.text('1'), findsNothing);
+  //
+  // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(tester.widget<Text>(counter).data, '3');
+  //
+  // // Verify that our counter has incremented.
+  // expect(find.text('0'), findsNothing);
+  // expect(find.text('1'), findsOneWidget);
+   });
+
 }
+
